b1085c4f5afaf25536941a328e5e87b5312c1785
Jairo Campos Ruiz <Jairocamposruiz1991@gmail.com>
2022-04-25 17:38:20 +0200

    Adding images and basic api

---
+++ pages/api/avo/index.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import DB from '@database';
+import enablePublicAccess from '@cors';
+
+const allAvos = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    await enablePublicAccess(req, res);
+
+    const db = new DB();
+    const allEntries = await db.getAll();
+    const lenght = allEntries.length;
+
+    res.status(200).json({
+      lenght,
+      data: allEntries,
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(500).end();
+  }
+};
+
+export default allAvos;
