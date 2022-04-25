c900d6ededf49e27b627669075d231bf96b3bf54
Jairo Campos Ruiz <Jairocamposruiz1991@gmail.com>
2022-04-25 17:04:10 +0200

    Adding taiwindcss, and creating context for cart

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import { AppProps } from 'next/app'
+import '../styles/global.css'
+
+import CartProvider from '@store/Cart'
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  // Aditional props
+  // Aditional layout
+  // Manejar errores - componentDidCatch
+  return (
+    <CartProvider>
+      <Component {...pageProps} />
+    </CartProvider>
+  )
+}
+
+export default MyApp
