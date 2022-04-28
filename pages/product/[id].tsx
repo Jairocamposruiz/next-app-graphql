7b4499f81640bc3d270ac7c523d0ad8583df6263
Jairo Campos Ruiz <Jairocamposruiz1991@gmail.com>
2022-04-28 15:09:03 +0200

    Bug

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -5,7 +5,7 @@ import { Layout } from '@components/Layout/Layout';
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const url = process.env.HOST;
-  const response = await fetch(`${url}/api/avo`);
+  const response = await fetch(`${ url }/api/avo`);
   const { data }: TAPIAvoResponse = await response.json();
 
   const paths = data.map(({ id }) => ({ params: { id } }));
@@ -17,7 +17,8 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const response = await fetch(`http://localhost:3000/api/avo/${ params?.id }`);
+  const url = process.env.HOST;
+  const response = await fetch(`${ url }/api/avo/${ params?.id }`);
   const product = await response.json();
 
   return {
@@ -34,7 +35,7 @@ interface Props {
 const ProductDetail = ({ product }: Props) => {
   return (
     <Layout>
-      {product === null ? null : <ProductSummary product={product} />}
+      { product === null ? null : <ProductSummary product={ product } /> }
     </Layout>
   );
 };
