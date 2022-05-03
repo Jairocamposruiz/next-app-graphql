ae187e1d50467c5bea9a70e058039ee6a75cd6bc
Jairo Campos Ruiz <Jairocamposruiz1991@gmail.com>
2022-05-03 12:10:10 +0200

    Adding prisma to project and adding sqlite database

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,20 @@
 import { ApolloServer } from 'apollo-server';
 import { join } from 'path';
 import { readFileSync } from 'fs';
+import { PrismaClient } from '@prisma/client';
+
 import resolvers from './resolvers';
+
+const orm = new PrismaClient();
 
 const typeDefs = readFileSync(join(__dirname, 'schema.graphql'), 'utf8');
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: {
+    orm
+  }
 });
 
 server.listen().then(({ url }) => console.log(`Server is running on ${ url }`));
