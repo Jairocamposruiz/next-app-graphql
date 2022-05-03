845c636e0ccc8f46b67f4bcee5b3414b8e022591
Jairo Campos Ruiz <Jairocamposruiz1991@gmail.com>
2022-05-03 12:34:41 +0200

    Adding apollo-express-server and apollo-server-core, changin index.ts adding route for static files

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,20 +1,42 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+// @ts-ignore
+import express from 'express';
+import { createServer } from 'http';
 import { join } from 'path';
 import { readFileSync } from 'fs';
 import { PrismaClient } from '@prisma/client';
 
 import resolvers from './resolvers';
 
+const typeDefs = readFileSync(join(__dirname, 'schema.graphql'), 'utf8');
 const orm = new PrismaClient();
 
-const typeDefs = readFileSync(join(__dirname, 'schema.graphql'), 'utf8');
+!(async function () {
+  const app = express();
+  const httpServer = createServer(app);
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: {
-    orm
-  }
-});
+  app.use('/images', express.static(join(__dirname, '../public')));
 
-server.listen().then(({ url }) => console.log(`Server is running on ${ url }`));
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: {
+      orm, //Aquí inyectamos nuestro ORM para tenerlo en el context de nuestros resolvers
+    },
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+  server.applyMiddleware({
+    app,
+    path: '/graphql',
+  });
+
+  await new Promise<void>((resolve) => {
+    httpServer.listen({ port: 4000 }, resolve);
+  })
+
+  console.log(`Server is running on http://localhost:4000${server.graphqlPath} `);
+})();
+
